2effdb57f469c200f3b12a8caf7c0d4f20316a7d
Paman-Shaikh <pamanshaikh.bscsf22@iba-suk.edu.pk>
2025-06-03 11:53:23 +0500

    code for controller, model and router

---
+++ BackEnd/controller.js
@@ -0,0 +1,46 @@
+const Book = require('../models/Book');
+
+exports.getAllBooks = async (req, res) => {
+  try {
+    const books = await Book.find();
+    res.json(books);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+exports.getBookById = async (req, res) => {
+  try {
+    const book = await Book.findById(req.params.id);
+    if (!book) return res.status(404).json({ message: 'Book not found' });
+    res.json(book);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+exports.createBook = async (req, res) => {
+  const book = new Book({
+    title: req.body.title,
+    author: req.body.author
+  });
+
+  try {
+    const newBook = await book.save();
+    res.status(201).json(newBook);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+exports.deleteBook = async (req, res) => {
+  try {
+    const deletedBook = await Book.findByIdAndDelete(req.params.id);
+    if (!deletedBook) return res.status(404).json({ message: 'Book not found' });
+    res.json({ message: 'Book deleted' });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+}; 
+ 
+ 
